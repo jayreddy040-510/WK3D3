767833b7149eda5d62896eae943ad09398c759b0
Jay Reddy <jayreddy@gmail.com>
2022-06-29 11:45:15 -0700

    done w/ warmup

--- recursion_exercises.rb
+++ recursion_exercises.rb
@@ -1,2 +1,15 @@
-def range
-end
+def range(start, last)
+
+    return [] if last < start
+    return [last] if last == start
+  
+        [start] + range(start+1,last) - [last]
+
+   
+    
+    
+end 
+
+print range(1,5)
+puts
+print range(5,9)
