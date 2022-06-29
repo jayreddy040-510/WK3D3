dfd94c4ab834fc10eb3920a2a714a9d6330cd96a
“NAME” <EMAIL@EMAIL.COM>
2022-06-29 12:22:31 -0700

    lunch update

--- recursion_exercises.rb
+++ recursion_exercises.rb
@@ -10,6 +10,29 @@ def range(start, last)
     
 end 
 
-print range(1,5)
-puts
-print range(5,9)
+# print range(1,5)
+# puts
+# print range(5,9)
+
+
+# recursion 1
+
+def exp(b, n)
+    return  1 if n == 0
+    return b  if n == 1
+    b * exp(b, n - 1)
+end
+
+def exp(b, n)
+    return  1 if n == 0
+    return b  if n == 1
+    
+    if n % 2 == 0
+        exp(b, n) = exp(b, n / 2) ** 2 
+    else
+        exp(b, n) = b * (exp(b, (n - 1) / 2) ** 2)
+    end
+end
+# exp(b, 0) = 1
+# exp(b, n) = b * exp(b, n - 1)
+
